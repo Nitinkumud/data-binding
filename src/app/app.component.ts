68531cfd5155aa4164c039607a7e2c2294ab1504
NITIN KUMAR <nitin.kumar@eco-mail.com>
2021-07-13 18:04:38 +0530

    pushing the code on github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DaterangepickerDirective } from 'ngx-daterangepicker-material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'date-range-picker';
+  imgUrl = "https://goodmorning-images.org/wp-content/uploads/2021/06/good-morning.jpg.webp";
+  fullName: string = "Nitin";
+
+  @ViewChild(DaterangepickerDirective, {static: true}) picker: DaterangepickerDirective;
+  selected: {startDate: moment.Moment, endDate: moment.Moment};
+  name = 'Angular';
+
+  // open date range picker
+  open() {
+    this.picker.open();
+  }
+
+// add alert on button click
+  onSave() {
+    alert('Event binding using button click')
+  }
 }
